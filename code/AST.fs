4dbea40fe4a108a442e4e003f7e033292dce7f19
Tashrique Ahmed <ta13@williams.edu>
2023-11-19 02:04:24 -0500

    Upated AST and Python Plot and Math Files Updates

--- code/AST.fs
+++ code/AST.fs
@@ -2,54 +2,36 @@ module AST
 
 
 (*
-    BNF Grammar
-    
-    <program> ::= <command> | <control> | <output>
+   BNF Grammar
 
-    <stock> ::= GOLD | SILVER | AMAZON | TESLA | AMDS | INTEL | MICROSOFT | FACEBOOK | GOOGLE | APPLE...
-    <transactionAmount> ::= <d><number> | <d>
+    <stock> ::= GOLD | SLVR | TSLA
+    <transactionAmount> ::= <d><transactionAmount> | <d>
     <d> ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
-    <command> ::= buy (<stock>, <transactionAmount>) | sell (<stock>, <transactionAmount>) | initialCapital(<transactionAmount>)
-    <control> ::= start | next | exit
-    <output> ::= graph(<graph>) | report(<report>)
-    <graph> ::= bargraph | timeseries
-    <report> ::= portfolio | statement | analysis
+
+    <command> ::= buy(<stock>, <transactionAmount>) | sell(<stock>, <transactionAmount>) | initialCapital(<transactionAmount>) //Dictionary and Array
+    
+    <line> ::= <command> | <output> 
+    <program> ::= <line> | <line><program>
+
+    <output> ::= output(<graph>)
+    <graph> ::= bargraph | timeseries | portfolio
 *)
 
 type Stock = 
-    | GOLD
-    | SILVER
-    | AMAZON
-    | TESLA
-    | AMD
-    | INTEL
-    | MICROSOFT
-    | FACEBOOK
-    | GOOGLE
-    | APPLE
+    | GOLD of string
+    | SLVR of string
+    | TSLA of string
 
-type TransactionAmount = TransactionAmount of int
+type Buy = {stock: Stock; amount: int}
+type Sell = {stock: Stock; amount: int}
+type InitialCapital = InitialCapital of int
 
-type Command =
-    | Buy of Stock * TransactionAmount
-    | Sell of Stock * TransactionAmount
-    | InitialCapital of TransactionAmount
+type Command = Buy | Sell | InitialCapital
 
-type Control =
-    | Next
-    | Exit
+type Output = Bargraph | Timeseries | Portfolio
 
-type Graph = BarGraph | TimeSeries
-
-type Report = Portfolio | Statement | Analysis
-
-type Output =
-    | Graph of Graph
-    | Report of Report
-
-type Program =
+type line =
     | Command of Command
-    | Control of Control
     | Output of Output
 
-type Canvas = Program list
+type Program = line list
